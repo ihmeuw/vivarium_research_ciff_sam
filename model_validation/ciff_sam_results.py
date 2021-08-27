108609d3227f72c9df834e4b7efd5b1172fe3633
Nathaniel Blair-Stahn <ndbs@uw.edu>
2021-08-27 15:05:30 -0700

    create ciff_sam_results.py file for processing results

---
+++ model_validation/ciff_sam_results.py
@@ -0,0 +1,8 @@
+"""
+Module providing functions and data structures for working with transformed Vivarium output
+from the CIFF SAM model.
+"""
+
+import collections
+import pandas as pd
+from vivarium_transformed_output import VivariumTransformedOutput
