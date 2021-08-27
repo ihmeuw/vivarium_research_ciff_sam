41fc06d57d60321be1365213cb5e1d9ce7b223a8
Nathaniel Blair-Stahn <ndbs@uw.edu>
2021-08-27 15:18:56 -0700

    add dataframe with model metadata

--- model_validation/ciff_sam_results.py
+++ model_validation/ciff_sam_results.py
@@ -6,3 +6,13 @@ from the CIFF SAM model.
 import collections
 import pandas as pd
 from vivarium_transformed_output import VivariumTransformedOutput
+
+models = pd.DataFrame(
+    [
+        [2.4, 'v2.4_corrected_fertility', '2021_08_03_15_08_32'],
+        [2.5, 'v2.5_stunting', '2021_08_05_16_17_12'],
+        [3.0, 'v3.0_sq_lns', '2021_08_16_17_54_19'],
+        [3.1, 'v3.1_sq_lns_stunting_stratified', '2021_08_24_10_28_32']
+    ],
+    columns=['model_number', 'model_name', 'run_id']
+)
