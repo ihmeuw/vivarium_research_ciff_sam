d74639a9fb4d60ee1a71cdb8b2d82698e07015f8
Nathaniel Blair-Stahn <ndbs@uw.edu>
2022-01-05 15:57:43 -0800

    add 'save_figures' function; add functionality to 'create_output_directories' function

--- model_validation/ciff_sam_plots.py
+++ model_validation/ciff_sam_plots.py
@@ -142,17 +142,23 @@ def convert_to_variable_name(string):
     """
     return re.sub('\W+|^(?=\d)', '_', string)
 
-def create_output_directories(username, model_name):
+def create_output_directories(username, model_name, home=True, j_drive=True, share=False):
     """
-    Creates directories for saving plots if they don't already exist.
+    Creates directories for saving plots if they don't already exist,
+    and returns a list of the directories.
     """
     project_vv_directory_name = 'ciff_malnutrition/verification_and_validation'
+    directories=[]
 
-    home_output_dir = f'/ihme/homes/{username}/vivarium_results/{project_vv_directory_name}/{model_name}'
-    share_output_dir = f'/share/scratch/users/ndbs/vivarium_results/{project_vv_directory_name}/{model_name}'
-    j_output_dir = f'/home/j/Project/simulation_science/{project_vv_directory_name}/{model_name}'
-
-    directories = [home_output_dir, j_output_dir] # Omit share_directory for now
+    if home:
+        home_output_dir = f'/ihme/homes/{username}/vivarium_results/{project_vv_directory_name}/{model_name}'
+        directories.append(home_output_dir)
+    if j_drive:
+        j_output_dir = f'/home/j/Project/simulation_science/{project_vv_directory_name}/{model_name}'
+        directories.append(j_output_dir)
+    if share:
+        share_output_dir = f'/share/scratch/users/ndbs/vivarium_results/{project_vv_directory_name}/{model_name}'
+        directories.append(share_output_dir)
 
     # Create the output directories if they don't exist
     # Note from Path.mkdir() documentation:
@@ -162,3 +168,18 @@ def create_output_directories(username, model_name):
     # in the correct (most permissive) permissions for the directories...
     for directory in directories:
         Path(directory).mkdir(mode=0o777, parents=True, exist_ok=True)
+    return directories
+
+def save_figures(figures: dict, directories):
+    """
+    Saves the figures to the specified directories as .pdf files.
+    `figures` is a dictionary mapping figure names (strings) to matplotlib Figure objects.
+        The figure names will be converted to valid filenames using the `convert_to_variable_name` function
+        before saving.
+    `directories` is a list of directories or a single directory name (str).
+    """
+    if isinstance(directories, str):
+        directories = [directories]
+    for figure_name, fig in figures.items():
+        for directory in directories:
+            fig.savefig(f"{directory}/{convert_to_variable_name(figure_name)}.pdf")
